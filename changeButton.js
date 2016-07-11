339ea8c2e7de6fef6bf318a07bb2b5a299287d48
LXJ <Inspire155@163.com>
2016-07-11 20:48:48 +0800

    add new js file

---
+++ changeButton.js
@@ -0,0 +1,10 @@
+<script type="test/javascript">
+function change(){
+	var demo=document.getElementsByTagName('button');
+    if(demo[0].innerHTML=='隐藏'){
+    demo[0].innerHTML='显示';}
+	else{
+    demo[0].innerHTML='隐藏';
+	}
+}
+</script>
